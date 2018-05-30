736ec5be777c95eff75e80a73fc9d34bb3cf2c12
Luiz Américo <luizamericop@gmail.com>
2018-05-30 19:57:26 -0300

    Configure default views by default

---
+++ src/bootstrap-views.js
@@ -0,0 +1,118 @@
+import {Model} from 'backbone'
+import {View} from 'backbone.marionette'
+
+const defaultCaptions = {
+  ok: 'OK',
+  cancel: 'Cancel',
+  no: 'No',
+  yes: 'Yes'
+}
+
+const ModalView = View.extend({
+  initialize (options = {}) {
+    this.model = new Model(options)
+  },
+
+  triggers: {
+    'click .btn-primary': 'confirm',
+    'click .btn-default': 'cancel',
+    'click .close': 'cancel'
+  },
+
+  events: {
+    submit: 'submit'
+  },
+
+  submit (e) {
+    e.preventDefault()
+    var val = this.$('input').val()
+    this.trigger('submit', val)
+  }
+})
+
+// simple renderer for deafult views
+const renderer = (template, data) => template(data)
+
+const LayoutView = ModalView.extend({
+  className: 'modal fade',
+
+  attributes: {
+    'tabindex': -1,
+    'role': 'dialog'
+  },
+
+  template: function renderLayout () {
+    return `
+    <div class="modal-dialog">
+      <div class="modal-content"></div>
+    </div>
+    `
+  }
+}).setRenderer(renderer)
+
+const AlertView = ModalView.extend({
+  template: function renderAlert (data) {
+    return `
+    <div class="modal-header">
+      <button type="button" class="close" aria-hidden="true">&times;</button>
+      <h4 class="modal-title">${data.title}}</h4>
+    </div>
+
+    <div class="modal-body">
+      <p>${data.text}</p>
+    </div>
+
+    <div class="modal-footer">
+      <button type="button" class="btn btn-primary">${defaultCaptions.ok}</button>
+    </div>
+    `
+  }
+}).setRenderer(renderer)
+
+const PromptView = ModalView.extend({
+  tagName: 'form',
+
+  template: function renderPrompt (data) {
+    return `
+    <div class="modal-header">
+      <button type="button" class="close" aria-hidden="true">&times;</button>
+      <h4 class="modal-title">${data.title}</h4>
+    </div>
+
+    <div class="modal-body">
+      <div class="form-group">
+        <label for="modal__input--prompt">${data.text}</label>
+        <input id="modal__input--prompt" class="form-control" type="text">
+      </div>
+    </div>
+
+    <div class="modal-footer">
+      <button type="button" class="btn btn-default">${defaultCaptions.cancel}</button>
+      <button type="submit" class="btn btn-primary">${defaultCaptions.ok}</button>
+    </div>
+    `
+  }
+}).setRenderer(renderer)
+
+const ConfirmView = ModalView.extend({
+  template: function renderConfirm (data) {
+    return `
+    <div class="modal-header">
+      <button type="button" class="close" aria-hidden="true">&times;</button>
+      <h4 class="modal-title">${data.title}</h4>
+    </div>
+    
+    <div class="modal-body">
+      <p>${data.text}</p>
+    </div>
+    
+    <div class="modal-footer">
+      <button type="button" class="btn btn-default">${defaultCaptions.no}</button>
+      <button type="button" class="btn btn-primary">${defaultCaptions.yes}</button>
+    </div>      
+    `
+  }
+
+}).setRenderer(renderer)
+
+export {ModalView, AlertView, LayoutView, ConfirmView, PromptView, defaultCaptions}
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,116 +1,18 @@
 import ModalService from './service'
-import {Model} from 'backbone'
-import {View, Region, mergeOptions} from 'backbone.marionette'
-
-const defaultCaptions = {
-  ok: 'OK',
-  cancel: 'Cancel',
-  no: 'No',
-  yes: 'Yes'
-}
-
-const defaultTemplates = {
-  LayoutView: function renderLayout () {
-    return `
-    <div class="modal-dialog">
-      <div class="modal-content"></div>
-    </div>
-    `
-  },
-
-  AlertView: function renderAlert (data) {
-    return `
-    <div class="modal-header">
-      <button type="button" class="close" aria-hidden="true">&times;</button>
-      <h4 class="modal-title">${data.title}}</h4>
-    </div>
-
-    <div class="modal-body">
-      <p>${data.text}</p>
-    </div>
-
-    <div class="modal-footer">
-      <button type="button" class="btn btn-primary">${defaultCaptions.ok}</button>
-    </div>
-    `
-  },
-
-  PromptView: function renderPrompt (data) {
-    return `
-    <div class="modal-header">
-      <button type="button" class="close" aria-hidden="true">&times;</button>
-      <h4 class="modal-title">${data.title}</h4>
-    </div>
-
-    <div class="modal-body">
-      <div class="form-group">
-        <label for="modal__input--prompt">${data.text}</label>
-        <input id="modal__input--prompt" class="form-control" type="text">
-      </div>
-    </div>
-
-    <div class="modal-footer">
-      <button type="button" class="btn btn-default">${defaultCaptions.cancel}</button>
-      <button type="submit" class="btn btn-primary">${defaultCaptions.ok}</button>
-    </div>
-    `
-  },
-
-  ConfirmView: function renderConfirm (data) {
-    return `
-    <div class="modal-header">
-      <button type="button" class="close" aria-hidden="true">&times;</button>
-      <h4 class="modal-title">${data.title}</h4>
-    </div>
-    
-    <div class="modal-body">
-      <p>${data.text}</p>
-    </div>
-    
-    <div class="modal-footer">
-      <button type="button" class="btn btn-default">${defaultCaptions.no}</button>
-      <button type="button" class="btn btn-primary">${defaultCaptions.yes}</button>
-    </div>      
-    `
-  }
-}
-
-const renderer = (template, data) => template(data)
-
-const ModalView = View.extend({
-  initialize (options = {}) {
-    this.model = new Model(options)
-  },
-
-  triggers: {
-    'click .btn-primary': 'confirm',
-    'click .btn-default': 'cancel',
-    'click .close': 'cancel'
-  },
-
-  events: {
-    submit: 'submit'
-  },
-
-  submit (e) {
-    e.preventDefault()
-    var val = this.$('input').val()
-    this.trigger('submit', val)
-  }
-})
-
-function createDefaultView (className) {
-  return ModalView.extend({
-    template: defaultTemplates[className]
-  }).setRenderer(renderer)
-}
+import { View, Region, mergeOptions } from 'backbone.marionette'
+import { ModalView, LayoutView, AlertView, PromptView, ConfirmView, defaultCaptions } from './bootstrap-views'
 
 const viewClassesNames = ['LayoutView', 'AlertView', 'PromptView', 'ConfirmView']
 
 const BootstrapModalService = ModalService.extend({
+  LayoutView,
+  AlertView,
+  PromptView,
+  ConfirmView,
+
   _prepareViewClasses () {
     viewClassesNames.forEach(className => {
-      let ViewClass = this[className] || (this[className] = createDefaultView(className))
+      let ViewClass = this[className]
       if (typeof ViewClass !== 'function') {
         throw new Error(`ModalService: expected ${className} to be a template function or View class`)
       }
@@ -167,6 +69,12 @@ const BootstrapModalService = ModalService.extend({
       this.once('modal:hide', resolve)
       this.layout.$el.modal('hide')
     })
+  }
+},
+// class methods
+{
+  setCaptions (captions = {}) {
+    Object.assign(defaultCaptions, captions)
   }
 })
 
